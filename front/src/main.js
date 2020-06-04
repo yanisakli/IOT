91a5adf3b77038c28118db448b69bcbc66b8f694
Tilkeii <florian@gouarin.info>
2020-06-04 17:28:43 +0200

    update alarm

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import Buefy from 'buefy'
+import Buefy from "buefy";
 import App from "./App.vue";
 import router from "./router";
-import 'buefy/dist/buefy.css'
+import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
 
-Vue.use(Buefy)
+Vue.use(Buefy);
 
 new Vue({
   router,
--- front/src/views/Alarm.vue
+++ front/src/views/Alarm.vue
@@ -1,14 +1,71 @@
 <template>
   <div class="alarm">
-    <h1>Alarm</h1>
+    <div class="columns">
+      <div class="column">{{ currentTime }}</div>
+    </div>
+
+    <div v-if="alarmValidated" class="columns">
+      <div class="column">{{ alarm }}</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <b-button v-if="alarmValidated" type="is-danger">Supprimer</b-button>
+        <b-collapse v-if="!alarmValidated" :open="false">
+          <button class="button is-primary" slot="trigger">Add alarm</button>
+          <b-datetimepicker
+            v-model="alarm"
+            class="m-top"
+            rounded
+            placeholder="Click to select..."
+            icon="calendar-today"
+            horizontal-time-picker
+          ></b-datetimepicker>
+          <button class="button is-success m-top" @click="validateAlarm()">Valider</button>
+        </b-collapse>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Alarm"
-  };
+import { addSeconds } from "date-fns";
+
+export default {
+  data() {
+    return {
+      currentTime: new Date(),
+      alarm: null,
+      alarmValidated: false,
+      intervalId: null
+    };
+  },
+  created() {
+    console.log("[ALARM] mounted called");
+    this.intervalId = setInterval(this.updateTime, 1000);
+  },
+  destroyed() {
+    console.log("[ALARM] destroy called");
+    clearInterval(this.intervalId);
+  },
+  methods: {
+    updateTime() {
+      console.log("[ALARM] updateTime called");
+      this.currentTime = addSeconds(this.currentTime, 1);
+    },
+    validateAlarm() {
+      if(this.alarm) {
+        this.alarmValidated = true;
+      } else {
+        alert('Date is empty');
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
+.m-top {
+  margin-top: 10px;
+}
 </style>
